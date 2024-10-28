fad0fdd7e06daa93e2ffd64768342515e584fdbb
nafyssat <nafyssatamohamedhalim@gmail.com>
2024-10-28 17:11:35 +0100

    premier model avec sklearn

--- model.py
+++ model.py
@@ -1,16 +1,22 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
 
 # Load data
 melbourne_file_path = "data/melb_data.csv"
 melbourne_data = pd.read_csv(melbourne_file_path) 
-# print a summary of the data in Melbourne data
-# print(melbourne_data.describe())
-# print(melbourne_data.columns)
 
-predicate_y = melbourne_data.Price
+
+y = melbourne_data.Price
 
 # melbourne_data = melbourne_data.dropna(axis=0)
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]
-print(X.describe())
+
+melbourne_model=DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X,y)
+
+print("Making predictions for the following 5 houses:")
 print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
